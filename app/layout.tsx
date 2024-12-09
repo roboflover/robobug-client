e329e794d6ac6eef482a163456bc4e317698d910
Your Name <you@example.com>
2024-12-09 04:37:21 +0300

    ppp

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,24 +2,20 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { useEffect, useState } from "react";
 import Script from "next/script";
-import Head from "next/head";
+// import Head from "next/head";
 import { OrderProvider } from '@/app/context/OrderContext';
 import { AuthProvider } from "./context/AuthContext";
 
-export const metadata: Metadata = {
-  title: 'Робожук сервис 3D печати с доставкой',
-  description: 'Сервис 3D печати с доставкой',
-};
+// export const metadata: Metadata = {
+//   title: 'Робожук сервис 3D печати с доставкой',
+//   description: 'Сервис 3D печати с доставкой',
+// };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode; }>) {
 
   return (
     <html lang="en" suppressHydrationWarning={true}>
-      <Head>
-        {/* <script src="https://api-maps.yandex.ru/v3/?apikey=95af15ee-dcb7-4205-af45-90b027553738&lang=ru_RU"></script> */}
-        <title>Робожук</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+
       <body className="font-sans" id="root" suppressHydrationWarning={true} style={{ fontFamily: 'Arial, Helvetica, sans-serif' }}>
         {/* <Script id="metrika-counter" strategy="afterInteractive">
           {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
